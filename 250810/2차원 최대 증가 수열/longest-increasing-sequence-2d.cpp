49b4813435df9413b475eef0ca60eb2ae3161a55
JiHyuk Lee <144404281+PotatoDevel0per@users.noreply.github.com>
2025-08-10 00:05:42 +0900

    Title: 2차원 최대 증가 수열, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250810/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 250810/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -10,15 +10,16 @@ int jump(int r, int c){
     if(dp[r][c] != 0) return dp[r][c];
 
     dp[r][c] = 1;
-
-    for(int i = r+1; i < n; i ++) {
-        for(int j = c+1; j < m; j++) {
-            if(grid[r][c] < grid[i][j]) {
-                dp[r][c] = max(dp[r][c], 1 + jump(i,j));
+    
+    if(r + 1 < n && c + 1 < m) {
+        for(int i = r+1; i < n; i ++) {
+            for(int j = c+1; j < m; j++) {
+                if(grid[r][c] < grid[i][j]) {
+                    dp[r][c] = max(dp[r][c], 1 + jump(i,j));
+                }
             }
         }
     }
-
     return dp[r][c];
 }
 
