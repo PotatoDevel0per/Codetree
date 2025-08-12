f0f55c1a3d6c8e373cf7e7608e56809553c83613
JiHyuk Lee <144404281+PotatoDevel0per@users.noreply.github.com>
2025-08-12 19:00:40 +0900

    Title: 동전 거슬러주기, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250812/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int coin[100];
+int dp[101];
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < N; i++) {
+        cin >> coin[i];
+    }
+
+    // Please write your code here.
+    for(int i = 0; i < 101; i++) {
+        dp[i] = INT_MAX;
+    }
+    dp[0] = 0;
+
+    for(int i = 1; i <= M; i++) {
+        for(int j = 0; j < N; j++) {
+            if(i >= coin[j]) {
+                if(dp[i - coin[j]] == INT_MAX)
+                    continue;
+                
+                dp[i] = min(dp[i], dp[i-coin[j]] + 1);
+            }
+        }
+    }
+
+    int ans = dp[M];
+    if(ans == INT_MAX)
+        ans = -1;
+    cout << ans;
+
+    return 0;
+}
