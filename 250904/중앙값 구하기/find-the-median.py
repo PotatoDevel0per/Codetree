01708d50756fc00b46427d71225b8491e60d504a
JiHyuk Lee <144404281+PotatoDevel0per@users.noreply.github.com>
2025-09-04 16:48:23 +0900

    Title: 중앙값 구하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/중앙값 구하기/find-the-median.py
+++ 250904/중앙값 구하기/find-the-median.py
@@ -1,9 +1,4 @@
 arr = list(map(int, input().split()))
-for j in range(0, len(arr)) :
-    for i in range(0, len(arr)-1-j) :
-        if arr[i] > arr[i+1] :
-            tmp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = tmp
+arr.sort()
 
-print(arr[1])
+print(arr[len(arr)//2])
