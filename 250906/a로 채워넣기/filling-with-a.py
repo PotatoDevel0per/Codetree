3d0705dedbd32ec88ee76f10c8f4e89207f34007
JiHyuk Lee <144404281+PotatoDevel0per@users.noreply.github.com>
2025-09-06 19:52:17 +0900

    Title: a로 채워넣기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250906/a로 채워넣기/filling-with-a.py
+++ 250906/a로 채워넣기/filling-with-a.py
@@ -1,6 +1,9 @@
 s = input()
-s_list = list(s)
-s_list[1] = 'a'
-s_list[len(s_list) - 2] = 'a'
-s = "".join(s_list)
+# s_list = list(s)
+# s_list[1] = 'a'
+# s_list[len(s_list) - 2] = 'a'
+# s = "".join(s_list)
+# print(s)
+
+s = s[:1] + 'a' + s[2:len(s)-2] + 'a' + s[len(s)-1:]
 print(s)
