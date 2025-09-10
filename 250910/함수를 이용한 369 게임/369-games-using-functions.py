5018dd912de83a8e72b15388d7a2a05a48267d17
JiHyuk Lee <144404281+PotatoDevel0per@users.noreply.github.com>
2025-09-10 23:26:46 +0900

    Title: 함수를 이용한 369 게임, Time: 143ms, Memory: 21MB, Status: Passed - Codetree

--- 250910/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250910/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,8 +1,1 @@
-a, b = map(int, input().split())
-
-def contains_369(n):
-    return any(c in '369' for c in str(n))
-
-count = sum(1 for i in range(a, b + 1) if i % 3 == 0 or contains_369(i))
-
-print(count)
+print((lambda a, b: sum(1 for i in range(a, b + 1) if i % 3 == 0 or any(c in '369' for c in str(i))))(*map(int, input().split())))
