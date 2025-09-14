82aa5b5515a6052ef9e36a573cae7913e89a1714
JiHyuk Lee <144404281+PotatoDevel0per@users.noreply.github.com>
2025-09-14 21:48:59 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250914/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 250914/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -2,17 +2,10 @@ n1, n2 = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-# Please write your code here.
-def check_CPS(a, b) :
-    for i in range(n1) : 
-        if a[i] == b[0] :
-            if i + n2 - 1 >= n1 : 
-                return False
-    
-            for j in range(1, n2) :
-                if a[i+j] != b[j] :
-                    return False
-        
-    return True
+def check_CPS(a, b):
+    for i in range(n1 - n2 + 1):
+        if a[i:i+n2] == b:
+            return True 
+    return False
 
 print(f"{'Yes' if check_CPS(a,b) else 'No'}")
