b1bc72b792af5d0baf26379a4a0268efd951e3f6
JiHyuk Lee <144404281+PotatoDevel0per@users.noreply.github.com>
2025-10-10 19:43:38 +0900

    Title: 나누고 빼면서 합하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251010/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 251010/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -2,11 +2,11 @@ n, m = map(int, input().split())
 A = list(map(int, input().split()))
 
 # Please write your code here.
-_sum = 0
+_sum = A[0]
 while m != 1 :
-    _sum += A[m]
+    _sum += A[m-1]
     if m % 2 == 0 :
-        m /= 2
+        m //= 2
     else :
         m -= 1
 print(_sum)
